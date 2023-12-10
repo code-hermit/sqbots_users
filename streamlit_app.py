d2a9bca5f31ab7206f4da856a16881392a3ce34d
code-hermit <92649583+code-hermit@users.noreply.github.com>
2023-12-10 15:14:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,8 @@ for dat in brokerwise:
     for broker in brokerwise[dat]:
         all_brokers_set.add(broker)
         for strategy in brokerwise[dat][broker]:
+            if strategy.lower() in ['testtable']:
+                continue
             all_strategies_set.add(strategy)
             strategies_dic[dat][broker][strategy]=brokerwise[dat][broker][strategy]['users']
             lots_dic[dat][broker][strategy]=brokerwise[dat][broker][strategy]['lots']
@@ -91,14 +93,15 @@ def get_df(start_date=START_DATE,end_date=END_DATE,broker_list=all_brokers_set,s
     rlis={k:sum({w:sum(x.values()) for w,x in v.items()}.values()) for k,v in t_table.items()}
     df=pd.Series(rlis)
     df=df[(df.index>=start_date) & (df.index<=end_date)]
-
+    df.sort_index(inplace=True)
     bro_df=pd.DataFrame(bro_dic).T
     bro_df=bro_df[(bro_df.index>=start_date) &(bro_df.index<=end_date)]
     bro_df.fillna(0,inplace=True)
-
+    bro_df.sort_index(inplace=True)
     stra_df=pd.DataFrame(stra_dic).T
     stra_df=stra_df[(stra_df.index>=start_date) & (stra_df.index<=end_date) ]
     stra_df.fillna(0,inplace=True)
+    stra_df.sort_index(inplace=True)
 
     
 
@@ -118,7 +121,7 @@ end_date = np.datetime64(date_col2.date_input("End Date", END_DATE))
 
 u_df=get_users_df(start_date=start_date,end_date=end_date)
 
-st.plotly_chart(plotchart.line_plot_df(u_df,'Date','Users','Unique Users'))
+st.plotly_chart(plotchart.line_plot_df(u_df,'Date','Users','Unique Users for All Bots'))
 col1, col2, col3 = st.columns(3)
 counter=1
 selected_brokers_list=[k for k in all_brokers_set if brok_check_dic[k]]
@@ -129,6 +132,6 @@ for table_type in ['orders','lots','strategies']:
     counter+=1
     bro_df,stra_df,df=get_df(broker_list=selected_brokers_list,strategy_list=selected_strategies_list,table_type=table_type,start_date=start_date,end_date=end_date)
     cur_col.plotly_chart(plotchart.line_plot_df(df,'Date',table_type,table_type))
-    cur_col.plotly_chart(plotchart.bar_plot_df(bro_df,'Date','Brokers',table_type.upper()))
-    cur_col.plotly_chart(plotchart.bar_plot_df(stra_df,'Date','Strategies',table_type.upper()))
+    cur_col.plotly_chart(plotchart.bar_plot_df(bro_df,'Date',table_type.upper(),table_type.upper()))
+    cur_col.plotly_chart(plotchart.bar_plot_df(stra_df,'Date',table_type.upper(),table_type.upper()))
     
